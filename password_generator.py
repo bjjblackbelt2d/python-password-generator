2e73df3008d2380f665e37b2927cfc4ed3d12884
Seth Sacco <sethrsacco@hotmail.com>
2023-06-30 18:50:51 -0400

    workingcode

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,13 @@
 import string
 import random
+from csv import writer
 
 def password_generator(): 
     lower_case = string.ascii_lowercase
     upper_case = string.ascii_uppercase
     digits = string.digits
     symbols = string.punctuation
+    platform = input("Enter the name of the platform ; \n")
     pass_length = int(input("Enter the length of the password : \n"))
     s = []
     s.extend(list(lower_case))
@@ -15,5 +17,8 @@ def password_generator():
     random.shuffle(s)
     password = ("".join(s[0:pass_length]))
     print(password)
-    
+    passdata= [platform,password]
+    with open ('pass.csv','a') as f:
+        writedata = writer(f)
+        writedata.writerow(passdata)
 password_generator()
