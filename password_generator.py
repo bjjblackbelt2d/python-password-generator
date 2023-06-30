a5ea8a3f1909fe5618c033fb3e68bf0dbb9c2e93
Seth Sacco <sethrsacco@hotmail.com>
2023-06-30 18:00:00 -0400

    stringvar

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+import string
+import random
+
+def password_generator(): 
+    lower_case = string.ascii_lowercase
+    print(lower_case)
+    upper_case = string.ascii_uppercase
+    print(upper_case)
+    digits = string.digits
+    print(digits)
+    symbols = string.punctuation
+    print(symbols)
+    
+password_generator()
